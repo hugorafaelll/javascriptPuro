b877639229707e32b8ebc22adc4c8c3187a2f451
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-07-19 12:59:12 -0300

    estudo

---
+++ curso-de-js-roger-melo/resumo.js
@@ -0,0 +1,19 @@
+// template string ou templates literals 
+  
+// inserir funçoes dentro de strings utilizando acento ``
+
+// template strings serve para salvar documentos no html utilizando o js e inserindo funçoes dentro de um array 
+
+
+
+
+
+const cachorroMagro = "Joao"
+const autor = "pedro"
+const itens = "setenta"
+
+
+
+const postMessage = `o post ${cachorroMagro}, do autor ${autor}, tem varios livros ${itens} a serem lidos`
+
+console.log(postMessage)
