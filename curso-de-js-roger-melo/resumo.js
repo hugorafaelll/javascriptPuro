6dbad5693118fe252669b7f624d61b89c5a35088
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-07-22 12:43:41 -0300

    exercicios 22-7

--- curso-de-js-roger-melo/resumo.js
+++ curso-de-js-roger-melo/resumo.js
@@ -29,7 +29,7 @@ console.log(postMessage)
 
 // array lista de valores []
 // sobreescrever [2 , 'cavalo']  sobreescreve o valor do indice selecionado exemplo
-brazilianFoods[1] = "pao de queijo "
+// brazilianFoods[1] = "pao de queijo "
 
 
 // array propriedades e metodos 
@@ -51,3 +51,59 @@ brazilianFoods[1] = "pao de queijo "
 // .includes (@) verifica se tem o item pesquisado em algum lugar retornando bolean  usado em arrays e string 
 
 //  !== nao e igual 
+
+
+
+//controle de fluxo
+
+// foor loop ITERAÇÂO 
+
+// for loop   executar um pedaço de codigo repetidas vezes 
+//  for (repetição) {bloco do que deve ser executado naquele loop}
+
+// loop infinito no while pois ele nao tem incremento incialmente 
+// precisa ser adicionado inicalmente para nao entrar em loop infinito i++
+
+// while (condição) {bloco de codigo }
+
+const names = ['hugo' , 'joao', 'samara']
+let i =0
+
+while(i<=5){
+    console.log(i)
+    i++
+}
+
+for (let i = 0; i < 6; i++) {
+    console.log(i);
+    
+}
+
+while (i<11){
+    console.log(`esta e a ${[i]} vez que esta frase é exibida`)
+     i++
+}
+
+
+const arrays = [
+    [4,32,8],
+    [64,8,32],
+    [8, 32 ,16],
+    [2,8,4]
+]
+
+let arraysSum = 0 ;
+
+
+
+for (let i = 0; i < arrays.length; i++) {
+    for (let j = 0; j < arrays.length; j++) {
+        arraysSum = arraysSum + arrays[i][j] 
+    }
+}
+console.log(arraysSum)
+
+
+//oque foi feito : dentro de cada array I incremente um intem J ate houver i 
+
+
