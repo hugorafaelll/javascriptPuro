94e29da2a87538edb975556450b6f06af960c601
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-07-27 10:35:54 -0300

    function parameter/argment

---
+++ curso-de-js-roger-melo/resumoCapitulo3.js
@@ -0,0 +1,15 @@
+//hoisting puchar declaração pro topo do arquivo
+
+
+//declarar sempre as funções no topo do codigo 
+
+
+//function declaration
+function sayHi () {
+    console.log("ola")
+}
+
+//function expression nao sao issadas "hoisting"
+const showFood = function () {
+    console.log("pizza")
+}
