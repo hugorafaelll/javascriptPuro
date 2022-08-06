409c8dac09b0af21e77b38fba41ae5c0b7888ee8
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-08-06 12:46:09 -0300

    estudo map

--- curso-de-js-roger-melo/resumoCapitulo6.js
+++ curso-de-js-roger-melo/resumoCapitulo6.js
@@ -3,12 +3,12 @@
 //prevent defalt evitar que a pagina fique recarregando toda vez q for enviado formulario 
 
 
-const form = document.querySelector('.classe do formulario')
+// const form = document.querySelector('.classe do formulario')
 
-form.addEventListener('submit', event =>{
-    event.preventDefault() //evitar que a pagina fique atualizando
-    console.log(event.target.username.value) // como conseguir o valor do usuario
-})
+// form.addEventListener('submit', event =>{
+//     event.preventDefault() //evitar que a pagina fique atualizando
+//     console.log(event.target.username.value) // como conseguir o valor do usuario
+// })
 
 //regex - padrao de texto esperado pelo sistema 
 //site regex101.com 
@@ -20,3 +20,30 @@ form.addEventListener('submit', event =>{
 //    /^[a-zA-Z0-9]{6,10}$/   aceita qualquer letra minuscula maiuscula de 6 a 10 letras para dar match 
 
 //    /^.{6-10}$  o ponto representa qualquer letra ou numero ou caractere especial
+
+
+const form = document.querySelector('.signup-form')
+const feedback = document.querySelector('.feedback')
+
+form.addEventListener('submit', event =>{
+    event.preventDefault()
+
+    const username = event.target.username.value // cosneguir o valor do usuario
+    const usernameRegex = /^[a-zA-Z0-9]{6,10}$/
+    const isValidFild = usernameRegex.test(username) //verifica se atende as especificações
+
+
+    if(isValidFild){
+        feedback.textContent = 'o usuario foi validado'
+        return
+    } feedback.textContent ='os dados estao errados '
+})
+
+  //validação de formulario em tempo real 
+
+    //keyupevent   evento depois de clicar em alguma tecla 
+
+
+    form.username.addEventListener('keyup', event => {
+    
+    })
---
+++ curso-de-js-roger-melo/resumomodulo8.js
@@ -0,0 +1,47 @@
+///formas de iterar sobre array   
+
+
+// loops 
+// while e for 
+
+// methodos :
+//forEach , find e some 
+// methodos que executam uma função de call back para cada item do array 
+
+
+
+
+// 3 METHODOS DE ARRAYS MAIS  IMPORTANTES 
+
+// MAP
+
+//methodod de array para gerar um novo array com cada item transformado 
+
+const number = [1,2,3] // percorre cada item do array e executa uma transformação e gera um novo array com o item transformado 
+
+//number.map((item,index,array))   // esta função pode receber 3 parametros item index array  
+
+// dos 3 parametros so um e obrigatorio o ITEM 
+counter = 0
+const dobleNumber = number.map(item => item*23)
+
+console.log(dobleNumber)
+
+// exemplo : MODIFICAR UM ARRAY CASO OS PRODUTOS FOREM MAIS CAROS QUE 30
+
+const products =[
+    {naome:'cachorro magro', price:30},
+    {name:'cavalo manco', price:28},
+    {name:'lanche de cachorro gordo', price :16},
+    {name:'casa da praia', price:175}
+]
+
+const vendaProdutos = products.map(produto =>{
+    if(produto.price >=30){
+        return{name:produto.name,price:produto.price /2}
+    }
+     return produto
+})
+
+console.log(vendaProdutos)
+console.log(products)
